76885788d294a0ee1efc6cbb60a3549ea75ea61b
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-05-05 19:15:57 +0900

    signup page created

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>アカウントを登録</h2>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="名前" v-model="name">
+    <input type="email" required placeholder="メールアドレス" v-model="email">
+    <input type="password" required placeholder="パスワード" v-model="password">
+    <input type="password" required placeholder="パスワード（確認用）" v-model="passwordConfirmation">
+    <button>登録する</button>    
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const name = ref('')
+    const email = ref('')
+    const password = ref('')
+    const passwordConfirmation = ref('')
+
+    const handleSubmit = () => {
+      console.log({ name, email, password, passwordConfirmation })
+    }
+
+    return { name, email, password, passwordConfirmation, handleSubmit }
+  }
+}
+</script>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container welcome">
+    <p>ようこそ</p>
+    <signup-form />
+  </div>
+</template>
+
+<script>
+import SignupForm from "../components/SignupForm.vue"
+
+export default  {
+  components: { SignupForm },
+}
+</script>
+
+<style scoped>
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
